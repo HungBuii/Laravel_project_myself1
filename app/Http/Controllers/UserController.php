23da968be9abb28f71f321124aecf2f71098bafe
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2023-07-25 20:44:13 +0700

    commit 4th

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,6 +19,6 @@ class UserController extends Controller
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
         User::create($incomingFields);
-        return 'Hello from register';
+        return 'Hello from register'; // haha
     }
 }
