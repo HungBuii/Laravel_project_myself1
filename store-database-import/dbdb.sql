35cfcf1583119119d9ad245136561d0a0614cc8d
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2023-08-24 18:38:32 +0700

    commit 23rd
    update database file

--- store-database-import/dbdb.sql
+++ store-database-import/dbdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ourlaravelapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ourlaravelapp`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ourlaravelapp
@@ -173,7 +175,7 @@ CREATE TABLE `posts` (
   PRIMARY KEY (`id`),
   KEY `posts_user_id_foreign` (`user_id`),
   CONSTRAINT `posts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,7 +184,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'2023-07-26 04:20:37','2023-07-26 04:20:37','My first blog','The sky is blue.',2),(3,'2023-07-29 06:39:33','2023-07-29 06:39:33','My Second Post','Testing',1),(7,'2023-08-06 22:36:00','2023-08-11 03:20:28','Halo','123456',2),(9,'2023-08-07 01:23:04','2023-08-11 02:59:41','hihihi!!','12345679',1),(12,'2023-08-14 22:53:31','2023-08-14 22:53:31','con cec','123',2),(14,'2023-08-22 21:58:35','2023-08-22 21:58:35','Update homepage-feed','Can paginate',1);
+INSERT INTO `posts` VALUES (1,'2023-07-26 04:20:37','2023-07-26 04:20:37','My first blog','The sky is blue.',2),(3,'2023-07-29 06:39:33','2023-07-29 06:39:33','My Second Post','Testing',1),(7,'2023-08-06 22:36:00','2023-08-11 03:20:28','Halo','123456',2),(9,'2023-08-07 01:23:04','2023-08-11 02:59:41','hihihi!!','12345679',1),(12,'2023-08-14 22:53:31','2023-08-14 22:53:31','con cec','123',2),(14,'2023-08-22 21:58:35','2023-08-22 21:58:35','Update homepage-feed','Can paginate',1),(15,'2023-08-23 20:15:10','2023-08-23 20:15:10','dit me may','123',3);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -229,4 +231,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-23 22:07:41
+-- Dump completed on 2023-08-24 16:14:37
