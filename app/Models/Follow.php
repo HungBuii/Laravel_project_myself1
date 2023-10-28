51ef7eead5401154226720d1f5f7509cdb879c83
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2023-10-28 19:53:13 +0700

    commit 32nd
    update Follow.php

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -11,10 +11,10 @@ class Follow extends Model
 
     public function userDoingTheFollowing() {
         return $this->belongsTo(User::class, 'user_id');
-    } // 1 fan has '1 follow' with 1 idol
+    } // 'belonging' relationship
 
     public function userBeingFollowed() {
         return $this->belongsTo(User::class, 'followeduser');
-    } // 1 idol has '1 follow' by 1 fan
+    }
 
 }
